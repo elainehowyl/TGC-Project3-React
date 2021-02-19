a2583ffd7de1332c4ebe1beff823b123efa39674
elainehowyl <elaine_0110@hotmail.com>
2021-02-19 07:11:10 +0000

    manage to display menu

--- project3-the-burger-shop/src/pages/FoodMenu.js
+++ project3-the-burger-shop/src/pages/FoodMenu.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import Card from 'react-bootstrap/Card';
+
 export default class FoodMenu extends React.Component{
     state = {
         foodList:[],
@@ -14,11 +16,47 @@ export default class FoodMenu extends React.Component{
        })
        console.log("Response data is: ", this.state.foodList)
     }
+
+    renderMenu(){
+        let jsx = []
+        for(let food of this.state.foodList){
+            jsx.push(
+                <React.Fragment>
+                    <Card style={{width:"25%", height:"25%"}}>
+                        <Card.Body>
+                            <Card.Img variant="top" src={food.image_source}/>
+                            <Card.Text>
+                                <h6>{food.name}</h6>
+                                <h6>Price: {food.price}</h6>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </React.Fragment>
+            )
+        }
+        return jsx
+    }
+
     render(){
         return(
-          <React.Fragment>
-            <h1>Testing Menu</h1>
-          </React.Fragment>
+            <React.Fragment>
+                {this.renderMenu()}
+            </React.Fragment>
         )
     }
+
+    // render(){
+    //     return (
+    //         <React.Fragment>
+    //             <Card style={{width:"25%", height:"25%"}}>
+    //              <Card.Body>
+    //                 <Card.Img src="./images/burger_shop_logo.png"/>
+    //                <Card.Text>
+    //                   Some quick example text to build on the cdwqdwqdsdsadsadsadsadsdsadasdsadsadwdwward title. qwuihfuiwededewdewdewdewdewdewdewdwdeqhfiwqhiuqwhiuwhiqwhiuhhwfuihiufhewuifhewifiuewhfuihieuwhfuiewhfiuewhfuiewh
+    //                </Card.Text>
+    //              </Card.Body>
+    //             </Card>
+    //         </React.Fragment>
+    //     )
+    // }
 }
